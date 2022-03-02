e2519f31aba2a1b28dacbf5670c6bfaba9acea13
BashayerAlharbi <baal7013@colorado.edu>
2022-03-01 22:19:16 -0700

    -

--- Code/jensen_shannon_divergence.py
+++ Code/jensen_shannon_divergence.py
@@ -2,6 +2,7 @@ import scipy
 import numpy as np
 from scipy.spatial import distance
 
+#this is just a code we were trying on random data
 distance.jensenshannon([1.0, 0.0, 0.0], [0.0, 1.0, 0.0], 2.0)
 
 a = np.array([[1, 2, 3, 4],
@@ -12,3 +13,5 @@ b = np.array([[1, 2, 3, 4],
                [9, 10, 11, 12]])
 print(distance.jensenshannon(a, b, axis=0))
 #print(distance.jensenshannon(a, b, axis=1))
+#then maybe a for loop
+#we need to use Panda to map the features with the IPs
