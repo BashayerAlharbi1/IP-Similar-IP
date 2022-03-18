cc67698721376d7aaf158a5eb0a2cd867436df6f
Bashayer Alharbi <99237409+BashayerAlharbi1@users.noreply.github.com>
2022-03-18 16:44:09 -0600

    Update IP_mapping.py

--- Code/IP_mapping.py
+++ Code/IP_mapping.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 data_features = pd.read_csv('features.features') #featuer file
-data_raw = pd.read_csv('ft-v05.2021-03-13.060001-0700.csv') #Raw data
+data_raw = pd.read_csv('ft-v05.2021-03-13.060001-0700.csv') #Raw data #depends on Netflow data
 
 data_features.insert(0,'S_IP', data_raw.loc[:,'srcaddr'])
 data_features.insert(1,'D_IP', data_raw.loc[:,'dstaddr'])
 
-#it is better to add this part of code to the man K-means code instead to writing it to a csv file to avoid dealing with the data frame indexes
+#it is better to add this code to the man K-means code file instead of writing it to a csv file to avoid dealing with the data frame indexes
 data_features.to_csv('/home/baal7013/CNA/mapped_features')
 
 
