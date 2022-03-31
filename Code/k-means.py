7facce790d4a31b9fb082a0185bf5ed638044c48
Ahowe2022 <100383582+Ahowe2022@users.noreply.github.com>
2022-03-31 09:24:13 -0600

    Update k-means.py

--- Code/k-means.py
+++ Code/k-means.py
@@ -1,26 +1,18 @@
 #---------------------------------------------------------------------------------------------------------#
 ############################################### Libraries ################################################
 #---------------------------------------------------------------------------------------------------------#
-import numpy as np #do we need this? -BA
+import numpy as np
 import pandas as pd
-#import statsmodels.api as sm
 import matplotlib.pyplot as plt
-import seaborn as sns #do we need this? -BA
-sns.set() #???
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
-from sklearn import preprocessing
-#from sklearn.preprocessing import label_binarize
-#from sklearn.model_selection import train_test_split
-#from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
-import shap
 
 #---------------------------------------------------------------------------------------------------------#
 #################################### Preparing the Data for Analysis ######################################
 #---------------------------------------------------------------------------------------------------------#
-data = pd.read_csv('features03_26') #featuer file
-print(data.shape[0]) #should we delete some of the printing?
+
+#Read feature file
+data = pd.read_csv('data') 
 
 #Normalizing the data
 data_value = data.copy()
@@ -29,60 +21,35 @@ data_scaled = min_max_scaler.fit_transform(data_value)
 data_df_scaled = pd.DataFrame(data_scaled, columns=data.columns)
 print(len(data_df_scaled))
 
-# Drop the first column that only contains 0s
+#Drop the first column that only contains 0s
 data_df_scaled.drop(columns=data_df_scaled.columns[0],axis=1,inplace=True)
-
-
-#---------------------------------------------------------------------------------------------------------#
-################################### Selecting the Optimal Number of k ####################################
-#---------------------------------------------------------------------------------------------------------#
-
-#The elbow method to select the optimal number of k:
-wcss=[]
-for i in range(1,10): #tries to clustere the data using different value of k, starting from k=1 to k=10.
-    kmeans = KMeans(n_clusters =i, init='k-means++')
-    kmeans.fit(data_df_scaled)
-    wcss_iter = kmeans.inertia_
-    wcss.append(wcss_iter)
-number_clusters = range(1,10)
-plt.plot(number_clusters,wcss)
-plt.title('The Elbow Method')
-plt.xlabel('Number of clusters')
-plt.ylabel('WCSS')
-plt.savefig('Elbow.png') # to show the plot 
-#plt.show() # this function did not work with me, thus i needed to save the plot as an image
-plt.clf() # to clear the current figure
 
 #---------------------------------------------------------------------------------------------------------#
 ############################################# Kmeans Cluster ##############################################
 #---------------------------------------------------------------------------------------------------------#
-
-#Clustring (we should determine how many clusters we want to create)
+#Clustering: Determines how many clusters are used
 #For now we choose the optimal number produced by the elbow method
 kmeans = KMeans(n_clusters =6 , init='k-means++')
 kmeans.fit(data_df_scaled)
-
-
-#Clustring result 
+#Clustering result 
 identified_clusters = kmeans.fit_predict(data_df_scaled)
 data_with_clusters = data_df_scaled.copy()
 data_with_clusters['Clusters'] = identified_clusters
 frame = pd.DataFrame(data_with_clusters)
-print (frame['Clusters'].value_counts()) # Print the size of each cluster  
+#prints the size of each cluster  
+print (frame['Clusters'].value_counts()) 
 
 #---------------------------------------------------------------------------------------------------------#
 ################################ Principal component analysis (PCA)  #####################################
 #---------------------------------------------------------------------------------------------------------#
-
 pca_num_components = 2 # to determine the number of dimensions, for now, we choose 2 dimensions. 
 reduced_data = PCA(n_components=pca_num_components).fit_transform(data_with_clusters)
 results = pd.DataFrame(reduced_data,columns=['pca1','pca2'])
 sns.scatterplot(x="pca1", y="pca2", hue=data_with_clusters['Clusters'], data=results)
 plt.title('K-means Clustering with 2 dimensions')
+plt.figure(figsize=(10, 5))
 plt.savefig('pca_clusters.png')
 plt.clf()
-
-
 centroids = kmeans.cluster_centers_
 centroids = pd.DataFrame(centroids, columns=['DpktsSA','DpktsSV','DoctetsSA','DoctetsSV',
                                              'DpktsDA','DpktsDV','DoctetsDA','DoctetsDV','AveTimeV','AveTimeA'])
@@ -99,17 +66,12 @@ plt.clf()
 #plt.savefig('centroids.png')
 
 #---------------------------------------------------------------------------------------------------------#
-#################################### Mapping IPs along with  Featuers ####################################
+#################################### Mapping IPs along with Features ####################################
 #---------------------------------------------------------------------------------------------------------#
-
 data_raw = pd.read_csv('ft-v05.2021-03-13.060001-0700.csv') #Raw data #depends on Netflow file
 # add the IP addresses along with features and clusters 
-data_with_clusters.insert(0,'S_IP', data_raw.loc[:,'srcaddr']) #first columns is zeros
+data_with_clusters.insert(0,'S_IP', data_raw.loc[:,'srcaddr']) #first column is zeros
 data_with_clusters.insert(1,'D_IP', data_raw.loc[:,'dstaddr'])
-
-
 print(data_with_clusters.loc[data_with_clusters['Clusters'] == 1]) #print all the data entries that belongs to cluster 1
 print(data_with_clusters.loc[data_with_clusters['Clusters'] == 0]) #print all the data entries that belongs to cluster 0
-
 data_with_clusters.to_csv('Final_Mapped_Features') #this file contains the IP addresses along with their classes (e.g, class 0 or class 1)
-
